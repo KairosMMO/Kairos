d1cf03e223497ab4258685db123007f1ddb60944
Kairos Murilo Matos de Oliveira <48840751882@DC01.IFS.IFSULDEMINAS.EDU.BR>
2023-04-03 08:41:35 -0300

    aula 17

--- aula16/index.php
+++ aula16/index.php
@@ -1,3 +1,12 @@
+<?php
+
+include "dados.php";
+
+?>
+
+
+
+
 <!doctype html>
 <html lang="en">
   <head>
@@ -7,18 +16,18 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
   </head>
   <body>
-    <h1>Hello, world!</h1>
-<!-- carousel -->
-<div id="carouselExample" class="carousel slide">
+
+    <!--Carousel-->
+    <div id="carouselExample" class="carousel slide">
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <img src="imagens/foto1.jpg" class="d-block w-100"  alt="...">
+      <img src="imagens/batata.jpg" class="d-block w-100" alt="...">
     </div>
     <div class="carousel-item">
-      <img src="imagens/foto2.jpg" class="d-block w-100"  alt="...">
+      <img src="imagens/cachorro.png" class="d-block w-100" alt="...">
     </div>
     <div class="carousel-item">
-      <img src="imagens/foto3.jpg" class="d-block w-100"  alt="...">
+      <img src="imagens/cachorro.png" class="d-block w-100" alt="...">
     </div>
   </div>
   <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
@@ -30,37 +39,35 @@
     <span class="visually-hidden">Next</span>
   </button>
 </div>
-<!-- Final do carousel -->
+    <!--Final do Carousel-->
+    <!--Inicio do menu-->
 
-<!-- Inicio do MEnu -->
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
+    <a class="navbar-brand" href="#">Menu</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
+       
+      <?php 
+      $categorias = [];
+      foreach($produtos as $key => $value): 
+      if(! in_array($value["categoria"], $categorias)):
+        $categorias[] = $value["categorias"]; 
+        ?>
+         
+         <li class="nav-item">
+            <a class="nav-link" href="#">
+             <?php echo $value["categoria"]?> 
           </a>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
-          </ul>
         </li>
-        <li class="nav-item">
-          <a class="nav-link disabled">Disabled</a>
-        </li>
+        
+        <?php 
+      endif;
+      endforeach; 
+      ?>
       </ul>
       <form class="d-flex" role="search">
         <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
@@ -69,14 +76,32 @@
     </div>
   </div>
 </nav>
-<!-- final do menu -->
+     <!--Final do menu-->
 
-<!-- lista de produtos -->
-<div class="container">
-    <div class="row row-cols-4">
+<hr>
+    
+     <!--Lista de Produtos-->
+    <div class="container">
+        <div class="row row-cols-4 g-4">
+          <?php
+          foreach($produtos as $key => $produto){
+          
+         ?>
+         <div class="col">
+        <div class="card">
+  <img src="<?php echo $produto["foto"];?>" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title"><?php echo $produto["nome"];?></h5>
+    <a href="#" class="btn btn-primary">Ver produto</a>
+    </div>
+  </div>
 </div>
-<!-- final da lista de produtos-->
-
+  <?php } ?>
+        </div>
+    </div>
+     <!--Final da lista de Produtos-->
+     <hr>
+   
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.bundle.min.js" integrity="sha384-qKXV1j0HvMUeCBQ+QVp7JcfGl760yU08IQ+GpUo5hlbpg51QRiuqHAJz8+BrxE/N" crossorigin="anonymous"></script>
   </body>
 </html>
